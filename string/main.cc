b5a389625a7c01ff01f9154de479d050737fe57b
xuesuc <xuesuc@163.com>
2015-05-17 21:27:10 +0800

    complete string class

--- string/main.cc
+++ string/main.cc
@@ -1,15 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 #include "qh_string.h"
-
 
 int main(int argc, char* argv[])
 {
-    //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
-    //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+	//TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
+	//TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
+	qh::string test1;
+	assert(NULL == test1.c_str());
+	assert(0 == test1.size());
+
+	char *p_test = "hello world"; 
+
+	qh::string test2(p_test);
+	assert(0 == strcmp(p_test, test2.c_str()));
+	assert(11 == test2.size());
+
+	qh::string test3(p_test, 5);
+	assert(0 == strcmp("hello", test3.c_str()));
+	assert(5 == test3.size());
+
+	qh::string test4(p_test, 20);
+	assert(0 == strcmp(p_test, test4.c_str()));
+	assert(11 == test4.size());
+
+	qh::string test5(test4);
+	assert(0 == strcmp(p_test, test5.c_str()));
+	assert(11 == test5.size());
+
+	test5 = test3;
+	assert(0 == strcmp("hello", test3.c_str()));
+	assert(5 == test3.size());
+
+	test5 = test5;
+	assert(0 == strcmp("hello", test3.c_str()));
+	assert(5 == test3.size());
+
+	assert(NULL == test5[20]);
+	assert('h' == *test5[0]);
+
+	qh::string test6(NULL);
+	assert(NULL == test6.data());
+	assert(0 == test6.size());
+
+	qh::string test7(test6);
+	assert(NULL == test7.data());
+	assert(0 == test7.size());
+
+	test7 = test6;
+	assert(NULL == test7.data());
+	assert(0 == test7.size());
+
 
 #ifdef WIN32
-    system("pause");
+	system("pause");
 #endif
 
 	return 0;
--- string/qh_string.cc
+++ string/qh_string.cc
@@ -4,53 +4,118 @@
 
 namespace qh
 {
-    // TODO 将在这里写实现代码
+	// TODO 将在这里写实现代码
+	void* my_molloc(int size){
+		void* p = malloc(size);
+		if(NULL != p){
+			return p;
+		}
+		else{
+			printf("out of memory");
+			exit(-1);
+		}
+	}
 
-    string::string()
-        : data_(NULL), len_(0)
-    {
-    }
+	string::string()
+		: data_(NULL), len_(0)
+	{
+	}
 
-    string::string( const char* s )
-    {
-    }
+	string::string( const char* s )
+	{
+		if(NULL != s){
+			int len = strlen(s);
+			data_ = (char*)my_molloc(sizeof(char) * len + 1);
+			strcpy(data_, s);
+			len_ = len;
+		}
+		else{
+			data_ = NULL;
+			len_ = 0;
+		}
+	}
 
-    string::string( const char* s, size_t len )
-    {
-    }
+	string::string( const char* s, size_t len )
+	{
+		if(NULL != s && len > 0){
+			int s_len = strlen(s);
+			if(s_len <= len){
+				data_ = (char*)my_molloc(sizeof(char) * s_len + 1);
+				strcpy(data_, s);
+				len_ = s_len;
+			}
+			else{
+				data_ = (char*)my_molloc(sizeof(char) * len + 1);
+				for(int i = 0; i < len; i++){
+					data_[i] = s[i];
+				}
+				data_[len] = 0;
+				len_ = len;
+			}
+		}
+		else{
+			data_ = NULL;
+			len_ = 0;
+		}
+	}
 
-    string::string( const string& rhs )
-    {
+	string::string( const string& rhs )
+	{
+		if(rhs.size() > 0){
+			data_ = (char*)my_molloc(sizeof(char) * rhs.size() + 1);
+			strcpy(data_, rhs.data_);
+			len_ = rhs.size();
+		}
+		else{
+			data_ = NULL;
+			len_ = 0;
+		}
+	}
 
-    }
+	string& string::operator=( const string& rhs )
+	{
+		if (&rhs != this){
+			free(this->data_);
+			if(rhs.size() > 0){
+				data_ = (char*)my_molloc(sizeof(char) * rhs.size() + 1);
+				strcpy(data_, rhs.data_);
+				len_ = rhs.size();
+			}
+			else{
+				data_ = NULL;
+				len_ = 0;
+			}
 
-    string& string::operator=( const string& rhs )
-    {
-        return *this;
-    }
+		}
+		return *this;
+	}
 
-    string::~string()
-    {
+	string::~string()
+	{
+		free(data_);
+	}
 
-    }
+	size_t string::size() const
+	{
+		return len_;
+	}
 
-    size_t string::size() const
-    {
-        return len_;
-    }
+	const char* string::data() const
+	{
+		return data_;
+	}
 
-    const char* string::data() const
-    {
-        return NULL;
-    }
+	const char* string::c_str() const
+	{
+		return data_;
+	}
 
-    const char* string::c_str() const
-    {
-        return NULL;
-    }
+	char* string::operator[]( size_t index )
+	{
+		if(len_ > index){
+			return data_ + index;
+		}
 
-    char* string::operator[]( size_t index )
-    {
-        return NULL;
-    }
+		return NULL;
+	}
 }
