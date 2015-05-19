524a2f82aeab5e61b428555c67934c99ce0cae36
xuesuc <xuesuc@163.com>
2015-05-19 20:10:59 +0800

    complete the vector class

--- vector/main.cc
+++ vector/main.cc
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <assert.h>
 #include "qh_vector.h"
 
 
@@ -9,7 +10,73 @@ int main(int argc, char* argv[])
 
     qh::vector<int> num_vect;
     num_vect.push_back(1);
+	num_vect.push_back(2);
+	assert(num_vect.size() == 2);
+	assert(num_vect[0] == 1);
+	assert(num_vect[1] == 2);
+	int test = 1;
+	for(qh::vector<int>::iterator it = num_vect.begin(); it != num_vect.end(); ++it){
+		assert(*it == test);
+		test++;
+	}
+	
+	qh::vector<int>::iterator num_it;
+	num_vect.push_back(3);
+	num_vect.push_back(4);
+	assert(num_vect.size() == 4);
+	num_it = num_vect.end();
+	assert(*(--num_it) == 4);
 
+	num_vect.pop_back();
+	assert(num_vect.size() == 3);
+	num_it = num_vect.end();
+	assert(*(--num_it) == 3);
+
+	assert(num_vect.empty() == false);
+
+	num_vect.clear();
+	assert(num_vect.size() == 0);
+	for(qh::vector<int>::iterator it = num_vect.begin(); it != num_vect.end(); ++it){
+		assert(false);
+	}
+
+	assert(num_vect.empty() == true);
+
+	qh::vector<int> num_vect_1(20,3);
+	assert(num_vect_1.empty() == false);
+	assert(num_vect_1.size() == 20);
+	for(qh::vector<int>::iterator it = num_vect_1.begin(); it != num_vect_1.end(); ++it){
+		assert(*it == 3);
+	}
+	
+	qh::vector<int> num_vect_2(0,3);
+	assert(num_vect_2.size() == 0);
+	assert(num_vect_2.empty() == true);
+
+	num_vect_2 = num_vect_1;
+	assert(num_vect_2.size() == 20);
+	assert(num_vect_2.empty() == false);
+	for(qh::vector<int>::iterator it = num_vect_2.begin(); it != num_vect_2.end(); ++it){
+		assert(*it == 3);
+	}
+
+	num_vect_2 = num_vect_2;
+	assert(num_vect_2.size() == 20);
+	assert(num_vect_2.empty() == false);
+	for(qh::vector<int>::iterator it = num_vect_2.begin(); it != num_vect_2.end(); ++it){
+		assert(*it == 3);
+	}
+	
+	num_vect_2.resize(10, 1);
+	assert(num_vect_2.size() == 10);
+
+	num_vect_2.resize(30,5);
+	assert(num_vect_2.size() == 30);
+	assert(num_vect_2[25] == 5);
+
+
+	
+	
 #ifdef WIN32
     system("pause");
 #endif
--- vector/qh_vector.h
+++ vector/qh_vector.h
@@ -1,30 +1,66 @@
 #ifndef QIHOO_VECTOR_H_
 #define QIHOO_VECTOR_H_
-
+#include <stdio.h>
 namespace qh
 {
     template<class T>
     class vector {
     public:
+		typedef T* iterator;
         //ctor
         vector()
-            : data_(NULL), size_(0)
+            : size_(0), reserve_size_(10)
         {
+			data_ = new T[10];
+			begin_ = end_ = data_;
         }
 
         explicit vector( size_t n, const T& value = T())
         {
-            data_ = new T[n];
-            for (size_t i = 0; i < n; i++)
-            {
-                data_[0] = value;
-            }
+			if(n > 0){
+				data_ = new T[n * 2];
+				reserve_size_ = n * 2;
+				size_ = n;
+				for (size_t i = 0; i < n; i++)
+				{
+					data_[i] = value;
+				}
+				begin_ = data_;
+				end_ = begin_ + size_;
+			}
+			else if(n == 0){
+				data_ = new T[10];
+				size_ = 0;
+				reserve_size_ = 10;
+				begin_ = end_ = data_;
+			}
         }
 
         vector<T>& operator=(const vector<T>& rhs)
         {
-            data_ = rhs.data_;
-            size_ = rhs.size_;
+			if(&rhs != this){
+				if (data_)
+				{
+					delete []data_;
+				}
+				if(rhs.size_ > 0){
+					data_ = new T[rhs.size_ * 2];
+					for(size_t i = 0; i < rhs.size(); i++){
+						data_[i] = rhs.data_[i];
+					}
+					size_ = rhs.size_;
+					reserve_size_ = rhs.size_ * 2;
+					begin_ = data_;
+					end_ = data_ + size_;
+				}
+				else{
+					data_ = new T[10];
+					size_ = 0;
+					reserve_size_ = 10;
+					begin_ = end_ = data_;
+				}
+			}
+			return *this;
         }
 
         //dtor
@@ -32,7 +68,7 @@ namespace qh
         {
             if (data_)
             {
-                delete data_;
+                delete []data_;
             }
         }
 
@@ -43,19 +79,109 @@ namespace qh
         }
 
         // set & get
-        T& operator[](size_t index);
+		T& operator[](size_t index){
+			if(size_ > index){
+				return data_[index];
+			}
+			else{
+				fprintf(stderr, "out of vector range");
+				exit(-1);
+			}
+		}
 
         // set
-        void push_back(const T& element);
-        void pop_back(const T& element);
-        void resize();
-        void reserve();
-        void clear();
-        void empty();
+		void push_back(const T& element){
+			if(reserve_size_ > size_){
+				data_[size_++] = element;
+				end_++;
+			}
+			else{
+				T* temp_data = data_;
+				data_ = new T[size_ * 2];
+				for(size_t i = 0; i < size_; i++){
+					data_[i] = temp_data[i];
+				}
+				reserve_size_ = size_ * 2;
+				data_[size_++] = element;
+				begin_ = data_;
+				end_ = begin_ + size_;
+			}
+		}
+
+		void pop_back(){
+			if(size_ > 0){
+				size_--;
+				end_--;
+			}
+		}
+
+		void resize(size_t n, const T& element){
+			if(size_ < n){
+				if(reserve_size_ < n){
+					T* old_data = data_;
+					data_ = new T[n * 2];
+					reserve_size_ = n * 2;
+					for(size_t i = 0; i < size_; i++){
+						data_[i] = old_data[i];
+					}
+					for(size_t i = size_; i < n; i++){
+						data_[i] = element;
+					}
+					size_ = n;
+					begin_ = data_;
+					end_ = begin_ + size_;
+					delete []old_data;
+				}
+				else{
+					for(size_t i = size_; i < n; i++){
+						data_[i] = element;
+					}
+					size_ = n;
+					end_ = begin_ + size_;
+				}
+			}
+			else{
+				size_ = n;
+				end_ = begin_ + size_;
+			}
+		}
+
+		void reserve(size_t n){
+			if(reserve_size_ < n){
+				T* old_data = data_;
+				data_ = new T[n];
+				for(size_t i = 0; i < size_; i++){
+					data_[i] = old_data[i];
+				}
+
+				delete old_data[];
+				begin_ = data_;
+				end = begin_ + size_;
+				reserve_size_ = n;
+			}
+		}
+		void clear(){
+			size_ = 0;
+			end_ = begin_;
+		}
+		bool empty() const{
+			return begin_ == end_;
+		}
+
+		T* begin(){
+			return begin_;
+		}
+
+		T* end(){
+			return end_;
+		}
 
     private:
         T*      data_;
+		T*      begin_;
+		T*      end_;
         size_t  size_;
+		size_t  reserve_size_;
     };
 }
 
